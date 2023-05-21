0862fef7f847cc37eecf9b70ec601a7343f67787
Jhon Munoz <jhonmunoz225205@correo.itm.edu.co>
2023-05-21 15:50:19 -0500

    Fix div by 0

--- MauiActivity/MVVM/ViewModel/CalculatorViewModel.cs
+++ MauiActivity/MVVM/ViewModel/CalculatorViewModel.cs
@@ -22,6 +22,7 @@ namespace MauiActivity.MVVM.ViewModel
         public ICommand ClickSubtract => new Command(SubtractNumber);
         public ICommand ClickMultiply => new Command(MultiplyNumber);
         public ICommand ClickDiv=> new Command(DivNumber);
+        public ICommand ClickPercentage => new Command(PercentageNumber);
         public ICommand ClickNumber => new Command(numberType);
         public ICommand ClickEqual => new Command(equalNumber);
         public ICommand ClickDelete => new Command(DeleteNumber);
@@ -55,9 +56,30 @@ namespace MauiActivity.MVVM.ViewModel
         }
         private void DivNumber(object obj)
         {
-            Num1 = Convert.ToDouble(History.ToString());
-            History = History += "/";
-            Oper = "/";
+            if (!(History[History.Length - 1]).Equals("/"))
+            {
+                Num1 = Convert.ToDouble(History.ToString());
+                History = History += "/";
+                Oper = "/";
+            }
+        }
+
+        private void PercentageNumber(object obj)
+        {
+            try
+            {
+                string number = History;
+                if (number != "0")
+                {
+                    decimal percentValue = Convert.ToDecimal(number);
+                    string result = (percentValue / 100).ToString("0.##");
+                    Result =  Double.Parse(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                History = "Error";
+            }
         }
 
         private void numberType(object obj)
@@ -81,7 +103,15 @@ namespace MauiActivity.MVVM.ViewModel
                         Result = Num1 - Num2;
                         break;
                     case "/":
-                        Result = Num1 / Num2;
+                        if (Num2 == 0)
+                        {
+                            Result = 0;
+                            History= "Error, Division por 0";
+                        }
+                        else
+                        {
+                            Result = Num1 / Num2;
+                        }
                         break;
                     case "*":
                         Result = Num1 * Num2;
